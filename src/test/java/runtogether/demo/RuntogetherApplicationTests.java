b46044392df960929d9b3e046f9c170d8ae510a4
kimnayoung <pfskdud@gmail.com>
2025-11-12 15:16:48 +0900

    Initial project setup

---
+++ src/test/java/runtogether/demo/RuntogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package runtogether.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
