dca7d637778e0a136ec0bc6f215306216df6ad92
kimnayoung <pfskdud@gmail.com>
2025-11-25 14:17:20 +0900

    Test: 회원가입부터 그룹 가입까지 전체 핵심 시나리오 테스트 완료

--- src/main/java/runtogether/server/dto/CourseDto.java
+++ src/main/java/runtogether/server/dto/CourseDto.java
@@ -2,8 +2,16 @@ package runtogether.server.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class CourseDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class RouteRequest {
+        private String startLocation; // 출발지 (예: "숙명여자대학교")
+        private String endLocation;   // 도착지 (예: "여의도공원")
+    }
 
     // ★ [수정됨] AI 추천 코스 응답용 DTO
     // 화면에 뿌려줄 모든 정보를 담아야 합니다.
